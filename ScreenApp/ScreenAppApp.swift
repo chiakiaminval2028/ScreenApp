aecfc32182c7e8e487444d3a022550c8acda0d46
Val <chia_kia_min_val_2028@mgs.sch.edu.sg>
2026-07-04 10:06:28 +0800

    Initial Commit

---
+++ ScreenApp/ScreenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenAppApp.swift
+//  ScreenApp
+//
+//  Created by Val on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ScreenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
